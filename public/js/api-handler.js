84c508a133b68893540ac5f09ac10aaadbdb7403
Cristian Perdomo <ferperdomo14@gmail.com>
2022-02-15 12:18:01 +0100

    api call

--- public/js/api-handler.js
+++ public/js/api-handler.js
@@ -1,3 +1,11 @@
-const url = `api.openweathermap.org/data/2.5/weather?q=${inputValue}&appid=${weatherApiKey}`
-fetch(url)
+class ApiHandler {
+    constructor() {
+        this.axiosApp = axios.create({
+            baseURL: `https://api.openweathermap.org/data/2.5/`
+        })
+    }
 
+    getInfo(inputValue) {
+        return this.axiosApp.get(`weather?q=${inputValue}&appid=9bd64e664d7d9b7b25e4a3a0eda152a9&units=metric`)
+    }
+}
