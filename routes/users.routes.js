57717a02a3b4a83bab14a89ab49acec57d083a32
Miguel Ortega Ward <miguel.owd@gmail.com>
2022-02-16 18:15:31 +0100

    links between favourites and detailed view

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -56,18 +56,12 @@ router.post('/myplaces/:cityName', (req, res, next) => {
 
 })
 
-// desde el form tenemos que enviar a este endpoint el id de la ciudad para añadir a favoritos
-// en este endpoint lo que queremos es añadir esa ciudad a las cities del user
-// 
-// una vez ese user tenga esa city en favs
-// en el perfil del user quieres mostrar los datos del tiempo de esas ciudades favoritas
-// es decir, en ese endpoint donde queires mostrar los datos del tiempo d elos favs
-// vas a tener que gestionar llamadas a la api
-// primero sacas los favoritos del current user y luego llamas a la api para que te muestre el tiempo de esos favs
 
 router.get('/eachplace/:cityName', (req, res, next) => {
 
     const { cityName } = req.params
+
+    const weatherApi = new weatherHandler()
 
     weatherApi
         .getForecast(cityName)
